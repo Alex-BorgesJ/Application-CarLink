fb3beac817253e61a8cb46ae6ecd8933e99c97b3
Vinicius Palmeira Farias <palmeiravinicius@gmail.com>
2024-05-17 14:39:32 -0300

    feat(pagina): Adiciona nova página de Ordem de Serviço frontend/backend

---
+++ Carlink/Paginas/CarLink_Ordem.aspx
@@ -0,0 +1,80 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Paginas/MasterPage.master" AutoEventWireup="true" CodeFile="CarLink_Ordem.aspx.cs" Inherits="Paginas_CarLink_Ordem" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+
+    <form runat="server">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title" style="color: #06184D">Ordem de serviços</h5>
+
+                <!-- Content Ordem serviços -->
+                <div class="row form-group">
+                    <div class="col-12 my-2">
+                        <asp:Label ID="lblMensagem" runat="server" CssClass="form-label text-danger"></asp:Label>
+                    </div>
+
+                    <div class="col-6 my-2">
+                        <asp:Label ID="lblDataEntra" runat="server" AssociatedControlID="txtBoxDataEntra" CssClass="form-label">Data de Entrada</asp:Label>
+                        <asp:TextBox ID="txtBoxDataEntra" runat="server" CssClass="form-control" TextMode="Date"></asp:TextBox>
+                    </div>
+
+                    <div class="col-6 my-2">
+                        <asp:Label ID="lblDataFinalizar" runat="server" AssociatedControlID="txtBoxDataFinalizar" CssClass="form-label">Data de Finalização</asp:Label>
+                        <asp:TextBox ID="txtBoxDataFinalizar" runat="server" CssClass="form-control" TextMode="Date"></asp:TextBox>
+                    </div>
+
+                    <div class="col-12 my-2">
+                        <asp:Label ID="lblDescri" runat="server" AssociatedControlID="txtBoxDescri" CssClass="form-label">Descrição</asp:Label>
+                        <asp:TextBox ID="txtBoxDescri" runat="server" CssClass="form-control" placeholder="...."></asp:TextBox>
+                    </div>
+
+                    <div class="col-6 my-2">
+                        <asp:Label ID="LabelPeca" runat="server" AssociatedControlID="TextBoxPeca" CssClass="form-label">Peças utilizadas</asp:Label>
+                        <asp:TextBox ID="TextBoxPeca" runat="server" CssClass="form-control" placeholder="Peças utilizadas..."></asp:TextBox>
+                    </div>
+
+                    <div class="col-6 my-2">
+                        <asp:Label ID="LabelQtd" runat="server" AssociatedControlID="TextBoxQtd" CssClass="form-label">Quantidade utilizada</asp:Label>
+                        <asp:TextBox ID="TextBoxQtd" runat="server" CssClass="form-control" placeholder="Quantidade..."></asp:TextBox>
+                    </div>
+
+                    <div class="col-6 my-2">
+                        <asp:Label ID="lblValServ" runat="server" AssociatedControlID="txtBoxValServ" CssClass="form-label">Valor de Serviço</asp:Label>
+                        <asp:TextBox ID="txtBoxValServ" runat="server" CssClass="form-control" placeholder="R$ 100.00"></asp:TextBox>
+                    </div>
+
+                    <div class="col-6 my-2">
+                        <asp:Label ID="lblValorTotal" runat="server" AssociatedControlID="txtBoxValorTotal" CssClass="form-label">Valor Total</asp:Label>
+                        <asp:TextBox ID="txtBoxValorTotal" runat="server" CssClass="form-control" placeholder="R$ 250.00"></asp:TextBox>
+                    </div>
+
+                    <div class="col-6 my-2">
+                        <asp:Label ID="lblVeiculo" runat="server" AssociatedControlID="txtBoxVeiculo" CssClass="form-label">Placa do veículo</asp:Label>
+                        <asp:TextBox ID="txtBoxVeiculo" runat="server" CssClass="form-control" placeholder="XXXXXXXXX"></asp:TextBox>
+                    </div>
+
+                    <div class="col-6 my-2">
+                        <asp:Label ID="lblMecanico" runat="server" AssociatedControlID="txtBoxMecanico" CssClass="form-label">Mecânico</asp:Label>
+                        <asp:TextBox ID="txtBoxMecanico" runat="server" CssClass="form-control" placeholder="John Doe"></asp:TextBox>
+                    </div>
+
+                    <div class="col-6 my-2">
+                        <asp:Label ID="lblIdCliente" runat="server" AssociatedControlID="dropDownIdCliente" CssClass="form-label">Cliente Associado</asp:Label>
+                        <asp:DropDownList ID="dropDownIdCliente" runat="server" CssClass="form-control" AppendDataBoundItems="true">
+                            <asp:ListItem Text="Selecione o Cliente" Value=""></asp:ListItem>
+                        </asp:DropDownList>
+                    </div>
+
+                    <div class="text-center col-12 mt-4">
+                        <asp:Button ID="btnSalvarOS" runat="server" Text="Salvar" CssClass="btn btn-primary" OnClick="btnSalvarOS_Click" />
+                        <asp:Button ID="btnCancelarOS" runat="server" Text="Cancelar" CssClass="btn btn-secondary" OnClick="btnCancelarOS_Click" />
+                    </div>
+
+                </div>
+                <!-- End Content Ordem serviços -->
+            </div>
+        </div>
+    </form>
+</asp:Content>
---
+++ Carlink/Paginas/CarLink_Ordem.aspx.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+
+public partial class Paginas_CarLink_Ordem : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            BindClientesDropDown();
+        }
+    }
+
+    private void BindClientesDropDown()
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["MySQLConnectionString"].ConnectionString;
+        using (MySqlConnection connection = new MySqlConnection(connectionString))
+        {
+            string query = "SELECT CLI_CODIGO, CLI_NOME FROM CLI_CLIENTE";
+            MySqlCommand command = new MySqlCommand(query, connection);
+            try
+            {
+                connection.Open();
+                MySqlDataReader reader = command.ExecuteReader();
+                dropDownIdCliente.DataSource = reader;
+                dropDownIdCliente.DataTextField = "CLI_NOME";
+                dropDownIdCliente.DataValueField = "CLI_CODIGO";
+                dropDownIdCliente.DataBind();
+            }
+            catch (Exception ex)
+            {
+                // Realiza a verificação e exibe a menssagem
+                Console.WriteLine("Erro ao carregar clientes: " + ex.Message);
+            }
+        }
+        dropDownIdCliente.Items.Insert(0, new ListItem("Selecione o Cliente", ""));
+    }
+
+    protected void btnSalvarOS_Click(object sender, EventArgs e)
+    {
+        lblMensagem.Text = string.Empty;
+
+        // Vai validar os campos
+        if (string.IsNullOrEmpty(txtBoxDataEntra.Text) || string.IsNullOrEmpty(txtBoxDataFinalizar.Text) ||
+            string.IsNullOrEmpty(txtBoxDescri.Text) || string.IsNullOrEmpty(txtBoxValServ.Text) ||
+            string.IsNullOrEmpty(txtBoxVeiculo.Text) || string.IsNullOrEmpty(txtBoxMecanico.Text))
+        {
+            lblMensagem.Text = "Todos os campos são obrigatórios.";
+            lblMensagem.CssClass = "text-danger";
+            return;
+        }
+
+        DateTime dataEntra;
+        DateTime dataFinalizar;
+        double valorServico;
+
+        if (!DateTime.TryParse(txtBoxDataEntra.Text, out dataEntra) || !DateTime.TryParse(txtBoxDataFinalizar.Text, out dataFinalizar))
+        {
+            lblMensagem.Text = "Data de Entrada e Data de Finalização devem ser válidas.";
+            lblMensagem.CssClass = "text-danger";
+            return;
+        }
+
+        if (!double.TryParse(txtBoxValServ.Text, out valorServico))
+        {
+            lblMensagem.Text = "Valor de Serviço deve ser um número válido.";
+            lblMensagem.CssClass = "text-danger";
+            return;
+        }
+
+        string descricao = txtBoxDescri.Text;
+        string placaVeiculo = txtBoxVeiculo.Text;
+        string mecanico = txtBoxMecanico.Text;
+
+        // verificação se o registro está cadastrado ou não
+        string clienteAssociado = GetClienteAssociado(placaVeiculo);
+        if (clienteAssociado == null)
+        {
+            lblMensagem.Text = "Veículo não está cadastrado.";
+            lblMensagem.CssClass = "text-danger";
+            return;
+        }
+
+        //Se estiver cadastrado, faz o link do cliente ao dropdown
+        dropDownIdCliente.SelectedValue = clienteAssociado;
+
+        string connectionString = ConfigurationManager.ConnectionStrings["MySQLConnectionString"].ConnectionString;
+        using (MySqlConnection connection = new MySqlConnection(connectionString))
+        {
+            string insertQuery = @"INSERT INTO ODS_ORDEM_SERVICO (ODS_DATACRIACAO, ODS_DATAFINALIZAR, ODS_DESCRICAO, ODS_VALORTOTAL, VEI_PLACA) 
+                                   VALUES (@DataCriacao, @DataFinalizar, @Descricao, @ValorTotal, @VeiculoPlaca)";
+            MySqlCommand command = new MySqlCommand(insertQuery, connection);
+            command.Parameters.AddWithValue("@DataCriacao", dataEntra);
+            command.Parameters.AddWithValue("@DataFinalizar", dataFinalizar);
+            command.Parameters.AddWithValue("@Descricao", descricao);
+            command.Parameters.AddWithValue("@ValorTotal", valorServico);
+            command.Parameters.AddWithValue("@VeiculoPlaca", placaVeiculo);
+            
+
+            try
+            {
+                connection.Open();
+                int result = command.ExecuteNonQuery();
+                if (result > 0)
+                {
+                    lblMensagem.Text = "Ordem de serviço cadastrada com sucesso.";
+                    lblMensagem.CssClass = "text-success";
+                }
+                else
+                {
+                    lblMensagem.Text = "Erro ao cadastrar a ordem de serviço.";
+                    lblMensagem.CssClass = "text-danger";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblMensagem.Text = "Erro: " + ex.Message;
+                lblMensagem.CssClass = "text-danger";
+            }
+        }
+    }
+
+    private string GetClienteAssociado(string placaVeiculo)
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["MySQLConnectionString"].ConnectionString;
+        using (MySqlConnection connection = new MySqlConnection(connectionString))
+        {
+            string query = "SELECT CLI_CODIGO FROM VEI_VEICULO WHERE VEI_PLACA = @PlacaVeiculo";
+            MySqlCommand command = new MySqlCommand(query, connection);
+            command.Parameters.AddWithValue("@PlacaVeiculo", placaVeiculo);
+
+            try
+            {
+                connection.Open();
+                object result = command.ExecuteScalar();
+                if (result != null)
+                {
+                    return result.ToString();
+                }
+                else
+                {
+                    return null; // Veículo não cadastrado
+                }
+            }
+            catch (Exception ex)
+            {
+                // Exibe a mensagem em caso de erro
+                Console.WriteLine("Erro ao verificar veículo: " + ex.Message);
+                return null;
+            }
+        }
+    }
+
+    protected void btnCancelarOS_Click(object sender, EventArgs e)
+    {
+        // Limpar todos os campos
+        txtBoxDataEntra.Text = "";
+        txtBoxDataFinalizar.Text = "";
+        txtBoxDescri.Text = "";
+        txtBoxValServ.Text = "";
+        txtBoxVeiculo.Text = "";
+        txtBoxMecanico.Text = "";
+        TextBoxPeca.Text = "";
+        TextBoxQtd.Text = "";
+        dropDownIdCliente.SelectedIndex = 0;
+
+        lblMensagem.Text = "";
+    }
+}
