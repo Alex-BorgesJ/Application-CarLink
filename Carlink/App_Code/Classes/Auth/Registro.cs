21eb4fc2edbba7c30b95a4c430d09db03554fa12
Vinicius Palmeira Farias <palmeiravinicius@gmail.com>
2024-06-13 22:38:36 -0300

    Feat(Registro) Nova página para registro

---
+++ Carlink/App_Code/Classes/Auth/Registro.cs
@@ -0,0 +1,125 @@
+using System;
+
+namespace CarLink.Classes.Auth
+{
+    /// Summary description for Veiculo
+    public class Registro
+    {
+        private string
+                      nome,
+                      sobrenome,
+                      email,
+                      senha,
+                      empresa,
+                      cpf,
+                      cnpj,
+                      tel;
+
+        public string Nome {
+            get { return nome; }
+            set
+            {
+                if (string.IsNullOrEmpty(value) || value.Length < 2)
+                {
+                    throw new ArgumentException("Nome tem que possuir no mínimo 2 caracteres");
+                }
+                nome = value;
+            }
+        }
+
+        public string Sobrenome {
+            get { return sobrenome; }
+            set
+            {
+                if (string.IsNullOrEmpty(value) || value.Length < 2)
+                {
+                    throw new ArgumentException("Sobrenome tem que possuir no mínimo 2 caracteres");
+                }
+                sobrenome = value;
+            }
+        }
+
+        public string Email {
+            get { return email; }
+            set
+            {
+                if (string.IsNullOrEmpty(value) || value.Length < 5)
+                {
+                    throw new ArgumentException("Email tem que possuir no mínimo 5 caracteres");
+                }
+                email = value;
+            }
+        }
+
+        public string Cpf {
+            get { return cpf; }
+            set
+            {
+                string cpfString = value.ToString();
+                if (cpfString.Length < 11)
+                {
+                    throw new ArgumentException("CPF deve possuir no mínimo 11 dígitos.");
+                }
+                cpf = value;
+            }
+        }
+
+        public string Cnpj {
+            get { return cnpj; }
+            set
+            {
+                string cnpjString = value.ToString();
+                if (cnpjString.Length < 11)
+                {
+                    throw new ArgumentException("CNPJ deve possuir no mínimo 11 dígitos.");
+                }
+                cnpj = value;
+            }
+        }
+
+        public string Tel {
+            get { return tel; }
+            set
+            {
+                string cpfString = value.ToString();
+                if (cpfString.Length < 8)
+                {
+                    throw new ArgumentException("Telefone deve possuir no mínimo 8 dígitos.");
+                }
+                tel = value;
+            }
+        }
+
+        public string Senha
+        {
+            get { return senha; }
+            set
+            {
+                if (string.IsNullOrEmpty(value) || value.Length < 8)
+                {
+                    throw new ArgumentException("Senha tem que possuir no mínimo 8 Digitos");
+                }
+                senha = value;
+            }
+
+        }
+
+        public string Empresa
+        {
+            get { return empresa; }
+            set
+            {
+                if (string.IsNullOrEmpty(value) || value.Length < 2)
+                {
+                    throw new ArgumentException("Empresa tem que possuir no mínimo 2 Digitos");
+                }
+                empresa = value;
+            }
+
+        }
+        public Registro() { 
+        
+        }
+        
+    }
+}
---
+++ Carlink/Paginas/CarLink_Registro.aspx.cs
@@ -0,0 +1,104 @@
+﻿using CarLink.Classes.Auth;
+using CarLink.Persistencia.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Paginas_CarLink_Cadastrar : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private void LimparCampos_Registro()
+    {
+        // Limpar todos os campos
+        txtBoxNomeRegistro.Text = "";
+        txtBoxSobrenomeRegistro.Text = "";
+        txtBoxEmpresa.Text = "";
+        txtBoxCNPJ.Text = "";
+        txtBoxCPF.Text = "";
+        txtBoxEmailCorp.Text = "";
+        txtBoxTelCorp.Text = "";
+        txtBoxSenha.Text = "";
+        txtBoxConfSenha.Text = "";
+
+        //Erros de campos
+        lblNomeError.Text = "";
+        lblSobrenomeError.Text = "";
+        lblEmpresaError.Text = "";
+        lblCnpjError.Text = "";
+        lblCpfError.Text = "";
+        lblEmailError.Text = "";
+        lblTelError.Text = "";
+        lblSenhaError.Text = "";
+        lblConfError.Text = "";
+
+        //Sucesso / Erro no cadastro
+        lblSucess.Text = "";
+        lblError.Text = "";
+    }
+
+    protected void btnSalvarCarros_Click(object sender, EventArgs e)
+    {
+        try {
+            
+
+            // Senha
+            if (!txtBoxConfSenha.Text.Equals(txtBoxSenha.Text))
+            {
+                lblConfError.Text = "Senha não compatível";
+                return;
+            }
+            else
+            {
+                lblConfError.Text = ""; // Limpar a mensagem de erro se não houver erro
+            }
+
+
+
+            //Copulando objeto de criação
+            Registro rg = new Registro();
+
+            rg.Nome = txtBoxNomeRegistro.Text;
+            rg.Sobrenome = txtBoxSobrenomeRegistro.Text;
+            rg.Empresa = txtBoxEmpresa.Text;
+            rg.Email = txtBoxEmailCorp.Text;
+            rg.Senha = txtBoxSenha.Text;
+            rg.Cnpj = txtBoxCNPJ.Text;
+            rg.Cpf = txtBoxCPF.Text;
+            rg.Tel = txtBoxTelCorp.Text;
+            
+
+            //Conexão com banco
+            RegistroBD registroBD = new RegistroBD();
+            int retorno = registroBD.Insert(rg);
+
+            if(retorno == 0)
+            {
+                lblSucess.Text = "Cadastro bem sucedido!";
+            }
+            else
+            {
+                lblSucess.Text = "Dando b.o " + rg.Nome + rg.Sobrenome + rg.Empresa + rg.Email + rg.Cpf + rg.Cnpj + rg.Tel + rg.Senha;
+            }
+
+
+        }
+        catch(Exception ex) {
+            lblError.Text = "ERRO! Verifique os campos digitados" + ex.Message;
+            return;
+        }
+
+
+    }
+
+    protected void btnCancelarCarros_Click(object sender, EventArgs e)
+    {
+        LimparCampos_Registro();
+    }
+}
